fe0fa1fbd1feb47f7f49409745af9ee73ef9d76d
Amanda Rolon <abrolon87@gmail.com>
2019-11-17 00:49:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,8 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
-  new = [starting_point]
-  counter = 0
+  new = []
+  counter = starting_point
   while counter < array.length
     new.push(yield(array[counter]))
     counter += 1
